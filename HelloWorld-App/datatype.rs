84e4be0ede8e421a2418ef5c1550b00db3264280
Sanjeev Tripurari <sanjeevtripurari@gmail.com>
2024-07-23 19:30:18 +0530

    initital commit rust lang

---
+++ HelloWorld-App/datatype.rs
@@ -0,0 +1,22 @@
+// rust datatypes
+//
+
+fn main(){
+
+    let user_name="Sanjeev";    // string type
+    let rating_float=4.5;       // float type
+    let _is_holiday=true;        // boolean
+
+    let _sum:i32 = 0;
+    let mark:isize=10;
+    let count:usize = 30;
+
+    let _sum=5-15;
+
+    println!("Name is {}", user_name);
+    println!("Boolean is {}", _is_holiday);
+    println!("Floating point: {}", rating_float);
+    println!("Sum of {} + {} = {}", 5,-15,_sum);
+    println!("mark is {} and count is {}", mark,count);
+
+}
